4f73b06a6f788ab9d216d05212ff1fe4f191bb93
hugomorenoo <hugomoreno1614@gmail.com>
2024-01-10 17:46:29 +0100

    final

--- routes/plato.py
+++ routes/plato.py
@@ -1,45 +1,45 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, HTTPException, status
 from config.bd import conn
 from schemas.plato import platosEntity, platoEntity
 from bson import ObjectId
 from models.plato import Plato
-from starlette.status import HTTP_204_NO_CONTENT
 
 plato = APIRouter()
 
-@plato.get('/', response_model=list[Plato], tags=["users"])
+@plato.get('/', response_model=list[Plato], tags=["platos"])
+@plato.get('/platos', response_model=list[Plato], tags=["platos"])
 def get_all_platos():
     return platosEntity(conn.hugomoreno.platos.find())
 
-@plato.post('/platos', response_model=Plato, tags=["users"])
+@plato.get('/platos/{id}', response_model=Plato, tags=["platos"])
+async def find_plato(id: str):
+    plato = conn.hugomoreno.platos.find_one({"_id": ObjectId(id)})
+    if plato:
+        return platoEntity(plato)
+    else:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+
+@plato.post('/platos', response_model=Plato, tags=["platos"])
 def create_plato(plato: Plato):
     nuevo_plato = dict(plato)
     id = conn.hugomoreno.platos.insert_one(nuevo_plato).inserted_id
     plato = conn.hugomoreno.platos.find_one({"_id": id})
     return platoEntity(plato)
 
-@plato.get('/platos/{id}', response_model=Plato, tags=["users"])
-async def find_plato(id: str):
+@plato.put('/platos/{id}', response_model=Plato, tags=["platos"])
+def edit_plato(id: str, nuevoPlato: Plato):
     plato = conn.hugomoreno.platos.find_one({"_id": ObjectId(id)})
     if plato:
-        return platoEntity(plato)
-    else:
-        return {"error": "Plato no encontrado"}
-
-@plato.put('/platos/{id}', response_model=Plato, tags=["users"])
-def edit_plato(id: str, plato: Plato):
-    plato = conn.hugomoreno.platos.find_one({"_id": ObjectId(id)})
-    if plato:
-        conn.hugomoreno.platos.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(plato)})
+        conn.hugomoreno.platos.update_one(plato, {"$set": dict(nuevoPlato)})
         return platoEntity(conn.hugomoreno.platos.find_one({"_id": ObjectId(id)}))
     else:
-        return {"error": "Plato no encontrado"}
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 
-@plato.delete('/platos/{id}', response_model=Plato, tags=["users"])
+@plato.delete('/platos/{id}', response_model=Plato, tags=["platos"])
 def delete_plato(id: str):
     plato = conn.hugomoreno.platos.find_one({"_id": ObjectId(id)})
     if plato:
         platoEntity(conn.hugomoreno.platos.find_one_and_delete({"_id": ObjectId(id)}))
         return platoEntity(plato)
     else:
-        return {"error": "Plato no encontrado"}
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
