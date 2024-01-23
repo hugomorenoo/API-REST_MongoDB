78d34d3545eb3fc0910f4d6981ce4e5e074a1129
hugomorenoo <hugomoreno1614@gmail.com>
2024-01-23 08:53:51 +0100

    Final

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.plato import plato
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API-REST para proyecto DWES. Hugo Moreno y Hugo Pérez",
@@ -7,3 +8,11 @@ app = FastAPI(
 )
 
 app.include_router(plato)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
